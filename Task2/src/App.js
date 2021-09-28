6747c95f8746891c1617562436bc929cdb6d2bc7
ayutivaluskar <71833435+ayutivaluskar@users.noreply.github.com>
2021-09-29 01:40:16 +0530

    Update App.js

--- Task2/src/App.js
+++ Task2/src/App.js
@@ -1,1 +1,44 @@
+import "./styles.css";
 
+import React, { useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState();
+  const getUsers = async () => {
+    const response = await fetch("https://reqres.in/api/users?");
+    const data = await response.json();
+    setUsers(data.data);
+  };
+
+  return (
+    <>
+      <header className="app">
+        <title> Task 2 </title>
+
+        <div className="btn" onClick={getUsers}>
+          Get Users
+        </div>
+      </header>
+      <div className="task">
+        <h3>YES2HEALTHYLIFE</h3>
+        <p>Change Your Life</p>
+      </div>
+      <div className="row">
+        {users?.map((current, index) => {
+          return (
+            <div className="card column" key={index}>
+              <img src={current.avatar} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h3 className="card-title">
+                  {current.first_name} {current.last_name}
+                </h3>
+                <h4 className="card-text">{current.email}</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+export default App;
